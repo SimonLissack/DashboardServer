663300348246a20039191cd5cd412dce818ed97c
Simon Lissack <lissack.simon@gmail.com>
2020-09-16 19:14:18 +0100

    Add a controller for showing leases from LanMonitor

---
+++ DashboardServer.WebApi/Controllers/LeasesController.cs
@@ -0,0 +1,41 @@
+﻿using DashboardServer.Services.LanMonitor.Domain.Router;
+using DashboardServer.Services.LanMonitor.Domain.Ssh;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DashboardServer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeasesController : ControllerBase
+    {
+        private readonly IDnsMasqLeaseService _dnsMasqLeaseService;
+        private readonly SshConfiguration _sshConfiguration;
+
+        public LeasesController(IDnsMasqLeaseService dnsMasqLeaseService, SshConfiguration sshConfiguration)
+        {
+            _dnsMasqLeaseService = dnsMasqLeaseService;
+            _sshConfiguration = sshConfiguration;
+        }
+
+        [HttpGet]
+        public async Task<LeasesRepresentation> Get()
+        {
+            var leases = _dnsMasqLeaseService.ListCurrentLeases();
+
+            return new LeasesRepresentation
+            {
+                HostName = _sshConfiguration.HostName,
+                Leases = await leases.ToListAsync()
+            };
+        }
+
+        public class LeasesRepresentation
+        {
+            public string HostName { get; set; }
+            public List<DnsMasqLease> Leases { get; set; }
+        }
+    }
+}
