aaaa9bf6ee69c65a8cf88d62eccb26ccdc4e1579
Simon Lissack <lissack.simon@gmail.com>
2020-09-16 19:09:15 +0100

    Added installer for lan monitor

---
+++ DashboardServer.Services.LanMonitor.Infrastructure/LanMonitorInstaller.cs
@@ -0,0 +1,20 @@
+﻿using DashboardServer.Services.LanMonitor.Domain.Router;
+using DashboardServer.Services.LanMonitor.Infrastructure.Router;
+using DashboardServer.Services.LanMonitor.Infrastructure.Ssh;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DashboardServer.Services.LanMonitor.Infrastructure
+{
+    public static class LanMonitorInstaller
+    {
+        public static IServiceCollection InstallLanMonitorService(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection
+                .AddTransient<SshClientFactory, SshClientFactory>()
+                .AddTransient<IDnsMasqLeaseService, DnsMasqLeaseService>();
+
+            return serviceCollection;
+        }
+    }
+}
