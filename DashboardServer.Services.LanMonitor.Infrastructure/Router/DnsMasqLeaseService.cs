877a08cfc26e1c90aa75723ab2037fce23099de3
Simon Lissack <lissack.simon@gmail.com>
2020-09-16 19:08:30 +0100

    Added infrastructure for downloading the leases file via SSH

---
+++ DashboardServer.Services.LanMonitor.Infrastructure/Router/DnsMasqLeaseService.cs
@@ -0,0 +1,45 @@
+﻿using DashboardServer.Services.LanMonitor.Domain.Router;
+using DashboardServer.Services.LanMonitor.Infrastructure.Ssh;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DashboardServer.Services.LanMonitor.Infrastructure.Router
+{
+    public class DnsMasqLeaseService : IDnsMasqLeaseService
+    {
+        const string LeasesFile = "/var/lib/misc/dnsmasq.leases";
+
+        private readonly SshClientFactory _sshClientFactory;
+
+        public DnsMasqLeaseService(SshClientFactory sshClientFactory)
+        {
+            _sshClientFactory = sshClientFactory;
+        }
+
+        public async IAsyncEnumerable<DnsMasqLease> ListCurrentLeases()
+        {
+            using var client = _sshClientFactory.CreateScpClient();
+            using var stream = new MemoryStream();
+            using var streamReader = new StreamReader(stream);
+
+            client.Connect();
+            client.Download(LeasesFile, stream);
+            stream.Seek(0, SeekOrigin.Begin);
+
+
+            while (!streamReader.EndOfStream)
+            {
+                var line = await streamReader.ReadLineAsync();
+                yield return ParseLine(line);
+            }
+        }
+
+        static DnsMasqLease ParseLine(string line)
+        {
+            var columns = line.Split(' ');
+
+            return new(int.Parse(columns[0]), columns[1], columns[2], columns[3], columns[4]);
+        }
+    }
+
+}
---
+++ DashboardServer.Services.LanMonitor.Infrastructure/Ssh/SshClientFactory.cs
@@ -0,0 +1,22 @@
+﻿using DashboardServer.Services.LanMonitor.Domain.Ssh;
+using Renci.SshNet;
+
+namespace DashboardServer.Services.LanMonitor.Infrastructure.Ssh
+{
+    public class SshClientFactory
+    {
+        private readonly SshConfiguration _configuration;
+
+        public SshClientFactory(SshConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        ConnectionInfo ConnectionInfo => new ConnectionInfo(_configuration.HostName, _configuration.User, new PrivateKeyAuthenticationMethod(_configuration.User, new PrivateKeyFile(_configuration.RsaKeyLocation)));
+
+        public ScpClient CreateScpClient()
+        {
+            return new ScpClient(ConnectionInfo);
+        }
+    }
+}
