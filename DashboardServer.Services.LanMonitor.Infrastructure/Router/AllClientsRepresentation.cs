0a7ad3e011f74cb44c3b6fa4cee27f980ee8626f
Simon Lissack <lissack.simon@gmail.com>
2020-09-20 16:11:48 +0100

    Add a representation to deserialize /jffs/nmp_cl_json.js

---
+++ DashboardServer.Services.LanMonitor.Infrastructure/Router/AllClientsRepresentation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DashboardServer.Services.LanMonitor.Infrastructure.Router
+{
+    /// <summary>
+    /// Represents the structure in /jffs/nmp_cl_json.js
+    /// </summary>
+    public class AllClientsRepresentation : Dictionary<string, KnownClient>
+    {
+        public IEnumerable<KnownClient> Devices => Values;
+    }
+
+    public record KnownClient(int Type, string Mac, string Name, string Vendor);
+}
