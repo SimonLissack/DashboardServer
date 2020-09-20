619b2b4143d2b5de6a548896a1cb9b7e5c1a8584
Simon Lissack <lissack.simon@gmail.com>
2020-09-20 16:09:58 +0100

    Change LeasesController namespace

--- DashboardServer.WebApi/Controllers/LanMonitor/LeasesController.cs
+++ DashboardServer.WebApi/Controllers/LanMonitor/LeasesController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DashboardServer.WebApi.Controllers
+namespace DashboardServer.WebApi.Controllers.LanMonitor
 {
     [Route("api/[controller]")]
     [ApiController]
