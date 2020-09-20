0cb935662fa00d9b80fcce6dfbcc348b7fca5d58
Simon Lissack <lissack.simon@gmail.com>
2020-09-20 15:45:53 +0100

    Add extension method for deserializing JSON which uses camelCase

---
+++ DashboardServer.Services.LanMonitor.Infrastructure/DeserializationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DashboardServer.Services.LanMonitor.Infrastructure
+{
+    public static class DeserializationExtensions
+    {
+        static JsonSerializerOptions SerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+        public static async Task<T> DeserializeAsync<T>(this Stream stream)
+        {
+            return await JsonSerializer.DeserializeAsync<T>(stream, options: SerializerOptions);
+        }
+    }
+}
